0126d698fc5740c0236cc65b555c259aa1b4e96b
18maherc <18maherc@gmail.com>
2021-12-07 22:10:30 -0500

    first baby step

--- my_bot.py
+++ my_bot.py
@@ -1,12 +1,15 @@
 # Terms of Service:
 # Don't copy this or I might cry
 
+# Imports because it's cool to have other stuff
+from server_token import getToken
 import discord
 
-# CLient (our bot)
+# Client - this is where the bot comes outta the womb
 client = discord.Client()
 
-# variables
+# Variables because calling stuff smaller stuff makes me a happy chappy
+server_token = getToken()
 gen_chat = client.get_channel(739822762062905487)
 music_chat = client.get_channel(744697370469728347)
 meme_chat = client.get_channel(739912364119556167)
@@ -18,7 +21,8 @@ scary_chat = client.get_channel(833023208902492200)
 bot_chat = client.get_channel(883740406469234718)
 gen_vchat = client.get_channel(739822762062905488)
 
-# work time
+# Work Time
 
-# Run the client on the server
-client.run('Nzk2NDg1MTA4NTg4MjE2Mzgw.X_Ymjw.2wpWn3DUJgESJpdkLBW-f35-GHw')
+
+# Run Time
+client.run(server_token)
