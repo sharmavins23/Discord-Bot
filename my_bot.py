3ddfd353346cca62f3cfd9ec78325c738e9011c3
18maherc <18maherc@gmail.com>
2021-12-07 21:53:28 -0500

    start

---
+++ my_bot.py
@@ -0,0 +1,24 @@
+# Terms of Service:
+# Don't copy this or I might cry
+
+import discord
+
+# CLient (our bot)
+client = discord.Client()
+
+# variables
+gen_chat = client.get_channel(739822762062905487)
+music_chat = client.get_channel(744697370469728347)
+meme_chat = client.get_channel(739912364119556167)
+tech_chat = client.get_channel(752552118577266709)
+hw_chat = client.get_channel(805806903656841237)
+pics_chat = client.get_channel(742423632524869732)
+spam_chat = client.get_channel(743540423586349196)
+scary_chat = client.get_channel(833023208902492200)
+bot_chat = client.get_channel(883740406469234718)
+gen_vchat = client.get_channel(739822762062905488)
+
+# work time
+
+# Run the client on the server
+client.run('Nzk2NDg1MTA4NTg4MjE2Mzgw.X_Ymjw.2wpWn3DUJgESJpdkLBW-f35-GHw')
