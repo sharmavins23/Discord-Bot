fe98c4c580c7032747209c61ae1b8ba41ffe43af
18maherc <18maherc@gmail.com>
2022-01-02 17:21:15 -0500

    version 1.2.1

--- my_bot.py
+++ my_bot.py
@@ -3,11 +3,13 @@
 # Don't copy this or I might cry
 
 # Imports because it's cool to have other stuff
-import discord
-from discord.ext import commands
 import random
+import discord
+from discord.ext import commands, tasks
+import spotipy
 import server_token
 import channel_tokens
+import spotify_tokens
 
 # Client - this is where the bot comes outta the womb
 client = commands.Bot(command_prefix=commands.when_mentioned_or(
@@ -15,6 +17,8 @@ client = commands.Bot(command_prefix=commands.when_mentioned_or(
 
 # Variables because calling stuff smaller stuff makes me a happy chappy
 server_token = server_token.get_server_token()
+#auth_manager = spotipy.SpotifyClientCredentials()
+#sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
 # --- Work Time ---
@@ -25,13 +29,21 @@ async def on_ready():
     await bot_chat.send('I am Pragosh. And I am the Messiah')
 
 
+# Pragosh's background behavior
+# @client.tasks.loop(seconds=1.0)
+# async def tribe_blend_checkup():
+#    bot_chat = client.get_channel(channel_tokens.get_bot_tchat)
+#    await bot_chat.send('lol')
+
+
 # Pragosh's responses to messages
 @client.event
 async def on_message(message):
-    if message.author.bot:
+    if message.author == client.user:  # don't want to check our own messages
         return
-    if message.author.id == 172002275412279296:  # instant replying to "Tatsu#8792"
-        await message.reply("Please stop abusing your girlfriend")
+    if message.author.id == 172002275412279296:  # instant replying to "Tatsu#8792" - this won't work
+        context = await client.get_context(message)
+        await context.message.reply("Please stop abusing your girlfriend")
     elif "POG" in message.content.upper():  # instant reacting to messages with pog
         will_pog_emoji = client.get_emoji(918323637398941716)
         await message.add_reaction(will_pog_emoji)
@@ -43,7 +55,7 @@ async def on_message(message):
 # Help Command
 @client.command(name="help")
 async def command_help(context, command=None):
-    if context.message.author.bot:
+    if context.message.author.bot:  # don't want to take commands from any bots
         return
     if command == None:
         help_message = 'List of current commands: \nbio \nrandomnumber \n\nFor further help, type \'Pragosh, help `command`\''
@@ -51,6 +63,8 @@ async def command_help(context, command=None):
         help_message = 'The bio command returns information about the currently released version of the bot'
     elif command == "randomnumber":
         help_message = 'To use the random number function, type \'Pragosh, randomnumber `starting integer` `ending integer` `count`\'\n\nNote: The count of generated numbers is optional. The default is 1'
+    elif command == "coinflip":
+        help_message = ''
 
     await context.message.reply(help_message)
 
@@ -58,7 +72,7 @@ async def command_help(context, command=None):
 # Bot Information Command
 @client.command(name="bio")
 async def bot_bio(context):
-    if context.message.author.bot:
+    if context.message.author.bot:  # don't want to take commands from any bots
         return
     bio_embed = discord.Embed(
         title="Hi!! I'm Pragosh.", description="This is all about me.", color=0x664616, url="https://github.com/18maherc/Discord-Bot")
@@ -74,7 +88,7 @@ async def bot_bio(context):
 # Random Number Generator Command
 @client.command(name="randomnumber")
 async def rand_num(context, lower_bound=0, upper_bound=0, count=0):
-    if context.message.author.bot:
+    if context.message.author.bot:  # don't want to take commands from any bots
         return
     random_numbers_str = ''
     if count == 0:
@@ -90,6 +104,8 @@ async def rand_num(context, lower_bound=0, upper_bound=0, count=0):
 # Coin Flip Command
 @client.command(name="coinflip")
 async def coin_flip(context):
+    if context.message.author.bot:  # don't want to take commands from any bots
+        return
     flip = random.randint(1, 2)
     if flip == 1:
         await context.message.reply(file=discord.File('FlippedHeads.png'))
