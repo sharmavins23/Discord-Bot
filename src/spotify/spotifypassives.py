2f0a321bbc510f0ea2551767fdbe428d8df3dc09
18maherc <18maherc@gmail.com>
2022-05-02 14:13:07 -0400

    repush

--- src/spotify/spotifypassives.py
+++ src/spotify/spotifypassives.py
@@ -59,7 +59,7 @@ class SpotifyPassives(commands.Cog):
             # check that the person game me the links I've asked for 4 times and counting
             if(tokens.get_person_data(person, 'onrepeat') is not None and tokens.get_person_data(person, 'repeatrewind') is not None):
                 # get the On Repeat playlist data dump
-                on_repeat = self.sp.playlist_items(
+                on_repeat = self.sp_auth.playlist_items(
                     tokens.get_person_data(person, 'onrepeat'), fields=None, limit=50, offset=0, market='US')
                 # randomly choose some track numbers to pick from OR
                 ORrands = [random.randint(0, 9), random.randint(
