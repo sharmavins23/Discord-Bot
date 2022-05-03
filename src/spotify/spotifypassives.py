48858382e605a8ce6e0c6f5dd668daf0250c7f03
18maherc <18maherc@gmail.com>
2022-05-02 22:59:41 -0400

    try this

--- src/spotify/spotifypassives.py
+++ src/spotify/spotifypassives.py
@@ -105,6 +105,6 @@ class SpotifyPassives(commands.Cog):
 
         # get our scraped songs and put them into a playlist
         self.sp_auth.playlist_replace_items(
-            tokens.get_TribeBlend2_ID(), list([str(track['ID']) for track in scraped_songs]))
+            tokens.get_TribeBlend2_ID(), [str(track['ID']) for track in scraped_songs][0])
 
         print(scraped_songs)
